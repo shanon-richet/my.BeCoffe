6885aaaa8a388fda39aa26fd3c1340f2866cfbcd
axel <axel@pop-os.localdomain>
2021-12-07 14:02:22 +0100

    session management (in progress)

--- server.mjs
+++ server.mjs
@@ -1,27 +1,45 @@
 import express from "express";
-import session from "express-session"
+import sessions from "express-session";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import  {v4 as uuidv4} from 'uuid';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT = 3000;
 const app = express();
 const oneDay = 1000 * 60 * 60 * 24;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//username and password mocks
+const myusername = 'tavormina.axel@gmail.com'
+const mypassword = '1234'
+
+let session;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
-    res.send('My app')
-})
-
 // Initiliaze the sessions
-app.use(session({
+app.use(sessions({
     secret: uuidv4(),
     saveUninitialized:true,
     cookie: { maxAge: oneDay },
     resave: false 
 }));
+
+app.use(cookieParser())
+
+// Login
+app.get('/',(req,res) => {
+    session = req.session;
+    if(session.userid){
+        res.send("Welcome User <a href=\'/logout'>click to logout</a>");
+    } else {
+        res.sendFile(__dirname + '/views/index.html')
+    }
+});
 
 // We tell our server to remain open, and listen to every incoming request
 app.listen(PORT, () => console.log(`server started`));

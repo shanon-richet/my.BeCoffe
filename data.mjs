c3ca8cb99d0f6be3ef93aff0494f35f8206d86da
hanjika <hanna.masanjika@gmail.com>
2021-12-09 09:47:20 +0100

    Added data.mjs

---
+++ data.mjs
@@ -0,0 +1,18 @@
+const data = [
+    ['Kévin', 'Monsieur', 'kevin.monsieur01@gmail.com', '&Y!m95', true],
+    ['Martin', 'Neyns', 'martin.neyns.pro@gmail.com', '^22Wwv', true],
+    ['Léon', 'Fashingabo', 'fashingaboleon@gmail.com', 'dP4%-N', true],
+    ['Mohammad', 'Azad', 'mohammadazad.bc@gmail.com', ':tA5HL', true],
+    ['Hanna', 'Masanjika', 'hanna.masanjika@gmail.com', '&Wz5=3', true],
+    ['Victor', 'Tardif', 'victor.tardif1gmail.com', '?Gek9g', true],
+    ['Esteban', 'Veraart', 'esteban.veraart@gmail.com', 'P.Mr9{', true],
+    ['Youssef', 'Akanni', 'youssefakanni2003@gmail.com', ']dA6wm', true],
+    ['Shanon', 'Richet', 'shanon.richet@gmail.com', 'm;Pf4p', true],
+    ['Dylan', 'Richardson', 'dylanrichardson1606@gmail.com', '7FJ+b{', true],
+    ['Aubry', 'Franquinet', 'aubry.franquinet@hotmail.com', 'Yz9^bt', true],
+    ['Axel', 'Tavormina', 'tavormina.axel@gmail.com', '=*Uw6,', true],
+    ['Kelian', 'Christophe', 'kelian.christophe@becode.org', 'y`7Ck,', false],
+    ['Michel', 'Plainchamp', 'michel.plainchamp@becode.org', '`T7Pzk', false]
+]
+
+export default data

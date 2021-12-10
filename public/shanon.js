b026c3e33a6d6d37328fa232e80488fcbe2ab6fd
Shanon <shanon.richet@gmail.com>
2021-12-10 12:16:46 +0100

    dashboard show only weekdays

--- public/shanon.js
+++ public/shanon.js
@@ -3,8 +3,9 @@ const recipeForm= document.querySelector('.recipe_form_hidden')
 
 const date = new Date();
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const days= ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-for (let i= 0; i < 15; i++) {
+for (let i= 0; i < 20; i++) {
     var calendar= document.createElement('section')
     var squares= document.createElement('p')
     calendar.appendChild(squares)
@@ -24,19 +25,25 @@ for (let i= 0; i < 15; i++) {
 }
 
 const square= document.querySelectorAll('p')
-square[0].innerText= new Date(date.getTime()).toLocaleDateString('fr-FR', options);
-square[1].innerText= new Date(date.getTime() + 86400000).toLocaleDateString('fr-FR', options);
-square[2].innerText= new Date(date.getTime() + 172000000).toLocaleDateString('fr-FR', options);
-square[3].innerText= new Date(date.getTime() + 259200000).toLocaleDateString('fr-FR', options);
-square[4].innerText= new Date(date.getTime() + 345600000).toLocaleDateString('fr-FR', options);
-square[5].innerText= new Date(date.getTime() + 432000000).toLocaleDateString('fr-FR', options);
-square[6].innerText= new Date(date.getTime() + 518400000).toLocaleDateString('fr-FR', options);
-square[7].innerText= new Date(date.getTime() + 604800000).toLocaleDateString('fr-FR', options);
-square[8].innerText= new Date(date.getTime() + 691200000).toLocaleDateString('fr-FR', options);
-square[9].innerText= new Date(date.getTime() + 777600000).toLocaleDateString('fr-FR', options);
-square[10].innerText= new Date(date.getTime() + 864000000).toLocaleDateString('fr-FR', options);
-square[11].innerText= new Date(date.getTime() + 950400000).toLocaleDateString('fr-FR', options);
-square[12].innerText= new Date(date.getTime() + 1036800000).toLocaleDateString('fr-FR', options);
-square[13].innerText= new Date(date.getTime() + 1123200000).toLocaleDateString('fr-FR', options);
-square[14].innerText= new Date(date.getTime() + 1209600000).toLocaleDateString('fr-FR', options);
 
+for (let i= 0; i< 5; i++) {
+  square[i].innerText= days[i]
+  square[i].classList.add('weekDays')
+}
+if (new Date(date.getDay() == 5)) {
+  square[5].innerText= new Date(date.getTime() - 345600000).toLocaleDateString('fr-Fr', options);//lundi
+  square[6].innerText= new Date(date.getTime() - 259200000).toLocaleDateString('fr-FR', options);//mardi
+  square[7].innerText= new Date(date.getTime() - 172000000).toLocaleDateString('fr-FR', options);//mercredi
+  square[8].innerText= new Date(date.getTime() - 86400000).toLocaleDateString('fr-FR', options);//jeudi
+  square[9].innerText= new Date(date.getTime()).toLocaleDateString('fr-FR', options);//vendredi
+  square[10].innerText= new Date(date.getTime() + 259200000).toLocaleDateString('fr-FR', options);//lundi
+  square[11].innerText= new Date(date.getTime() + 345600000).toLocaleDateString('fr-FR', options);
+  square[12].innerText= new Date(date.getTime() + 432000000).toLocaleDateString('fr-FR', options);//mercredi
+  square[13].innerText= new Date(date.getTime() + 518400000).toLocaleDateString('fr-FR', options);
+  square[14].innerText= new Date(date.getTime() + 604800000).toLocaleDateString('fr-FR', options);//vdd
+  square[15].innerText= new Date(date.getTime() + 864000000).toLocaleDateString('fr-FR', options);//lundi
+  square[16].innerText= new Date(date.getTime() + 950400000).toLocaleDateString('fr-FR', options);
+  square[17].innerText= new Date(date.getTime() + 1036800000).toLocaleDateString('fr-FR', options);//mercredi
+  square[18].innerText= new Date(date.getTime() + 1123200000).toLocaleDateString('fr-FR', options);
+  square[19].innerText= new Date(date.getTime() + 1209600000).toLocaleDateString('fr-FR', options);//vendredi
+}

f8c32bec3532b3b36f73b4c8cd3043b61940512a
Shanon <shanon.richet@gmail.com>
2021-12-15 10:07:53 +0100

    change dates of calendar

--- public/shanon.js
+++ public/shanon.js
@@ -4,7 +4,7 @@ const date = new Date();
 const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 20; i++) {
     const calendar = document.querySelector('.calendar')
     const squares = document.createElement('div')
     squares.classList.add('calendar-square')
@@ -23,12 +23,11 @@ for (let i = 0; i < 15; i++) {
         }
     }
 }
-
 const square = document.querySelectorAll('.calendar-square');
 
 for (let i = 0; i < 5; i++) {
   square[i].innerText = days[i]
-  square[i].classList.add('weekDays')
+  square[i].setAttribute('class', 'week-days')
 }
 
 if (date.getDay() == 5) {
@@ -75,15 +74,15 @@ if (date.getDay() == 3) {
   square[8].innerText= new Date(date.getTime() + 86400000).toLocaleDateString('fr-FR', options);//jeudi
   square[9].innerText= new Date(date.getTime() + 172000000).toLocaleDateString('fr-FR', options);//vendredi
   square[10].innerText= new Date(date.getTime() + 432000000).toLocaleDateString('fr-FR', options);//lundi
-  square[13].innerText= new Date(date.getTime() + 518400000).toLocaleDateString('fr-FR', options);//mardi
-  square[14].innerText= new Date(date.getTime() + 604800000).toLocaleDateString('fr-FR', options);//mercredi
-  square[15].innerText= new Date(date.getTime() + 864000000).toLocaleDateString('fr-FR', options);
-  square[16].innerText= new Date(date.getTime() + 950400000).toLocaleDateString('fr-FR', options);//vendredi
-  
-  square[17].innerText= new Date(date.getTime() + 1209600000).toLocaleDateString('fr-FR', options);//lundi
-  square[18].innerText= new Date(date.getTime() + 1123200000).toLocaleDateString('fr-FR', options);//mardi
-  square[19].innerText= new Date(date.getTime() + 1209600000).toLocaleDateString('fr-FR', options);
-
+  square[11].innerText= new Date(date.getTime() + 518400000).toLocaleDateString('fr-FR', options);//mardi
+  square[12].innerText= new Date(date.getTime() + 604800000).toLocaleDateString('fr-FR', options);//mercredi
+  square[13].innerText= new Date(date.getTime() + 691200000).toLocaleDateString('fr-FR', options);
+  square[14].innerText= new Date(date.getTime() + 777600000).toLocaleDateString('fr-FR', options);//vendredi
+  square[15].innerText= new Date(date.getTime() + 1036800000).toLocaleDateString('fr-FR', options);
+  square[16].innerText= new Date(date.getTime() + 1123200000).toLocaleDateString('fr-FR', options);
+  square[17].innerText= new Date(date.getTime() + 1209600000).toLocaleDateString('fr-FR', options);
+  square[18].innerText= new Date(date.getTime() + 1296000000).toLocaleDateString('fr-FR', options);//jeudi
+  square[19].innerText= new Date(date.getTime() + 1382400000).toLocaleDateString('fr-FR', options);
 }
 
 if (date.getDay() == 2) {
